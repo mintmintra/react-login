cf06f704d5de3afb64a686c521f9c1db81c139f0
mintmintra <mintracss019@gmail.com>
2022-07-01 23:24:44 +0700

    profile

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,12 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 function Login() {
+  const navigate = useNavigate()
+  const MySwal = withReactContent(Swal)
+
   const [inputs, setInputs] = useState({});
 
   const handleChange = (event) => {
@@ -11,7 +17,44 @@ function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(inputs);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "username": inputs.username,
+      "password": inputs.password,
+      "expiresIn": 60000
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://www.mecallapi.com/api/login", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        if (result.status === 'ok'){
+          MySwal.fire({
+            html: <i>{result.message}</i>,
+            icon: 'success'
+          }).then((value) => {
+            localStorage.setItem('token', result.accessToken)
+            navigate('/profile')
+          })
+        }else{
+          MySwal.fire({
+            html: <i>{result.message}</i>,
+            icon: 'error'
+          })
+        }
+      })
+      .catch(error => console.log('error', error));
+        console.log(inputs);
   }
 
   return (
--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 function Profile() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [user, setUser] = useState([]);
   return (
     <div>Profile</div>
   )
