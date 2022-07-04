69fa558a8d507117bdc0156891256bb3ee8ca0c2
mintmintra <mintracss019@gmail.com>
2022-07-04 16:38:59 +0700

    profile

--- src/Profile.js
+++ src/Profile.js
@@ -3,6 +3,25 @@ import {useState, useEffect} from 'react'
 function Profile() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+
+    var requestOptions = { 
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://www.mecallapi.com/api/auth/user", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => console.log('error', error));
+      }, [])
   return (
     <div>Profile</div>
   )
